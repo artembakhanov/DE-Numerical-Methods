fb2e24b3622ce1a306949dfd2b3bb45780761bb8
Artem Bakhanov <artembakhanov@gmail.com>
2019-11-03 21:12:54 +0300

    Add Runge-Kutta Method

---
+++ app/src/main/java/com/bakhanov/denumericalmethods/RungeKuttaMethod.kt
@@ -0,0 +1,13 @@
+package com.bakhanov.denumericalmethods
+
+class RungeKuttaMethod(override val equation: Equation) : IterativeMethod(equation) {
+    override fun next(xi: Double, yi: Double): Double {
+        val f = equation.function
+        val k1i = f(xi, yi)
+        val k2i = f(xi + step / 2, yi + step / 2 * k1i)
+        val k3i = f(xi + step / 2, yi + step / 2 * k2i)
+        val k4i = f(xi + step, yi + step * k3i)
+
+        return yi + step / 6 * (k1i + 2 * k2i + 2 * k3i + k4i)
+    }
+}
