d81af02e4dbd85304cb75bc971f06eea9d0c00c0
Artem Bakhanov <artembakhanov@gmail.com>
2019-11-06 15:54:45 +0300

    Change plots style

--- app/src/main/java/com/bakhanov/denumericalmethods/Activities/MainActivity.kt
+++ app/src/main/java/com/bakhanov/denumericalmethods/Activities/MainActivity.kt
@@ -5,11 +5,15 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bakhanov.denumericalmethods.NumericalMethods.Equation
 import com.bakhanov.denumericalmethods.NumericalMethods.Exception.NMArgumentException
+import com.bakhanov.denumericalmethods.NumericalMethods.Exception.NMDomainException
+import com.bakhanov.denumericalmethods.NumericalMethods.ScientificFormatter
 import com.bakhanov.denumericalmethods.R
 import com.bakhanov.denumericalmethods.Solver.Method
 import com.bakhanov.denumericalmethods.Solver.PlotData
 import com.bakhanov.denumericalmethods.Solver.Solver
+import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.formatter.LargeValueFormatter
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetBehavior.STATE_COLLAPSED
 import com.google.android.material.bottomsheet.BottomSheetBehavior.STATE_EXPANDED
@@ -46,6 +50,8 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "Please, enter valid numbers", Toast.LENGTH_LONG).show()
             } catch (e: NMArgumentException) {
                 Toast.makeText(this, e.message, Toast.LENGTH_LONG).show()
+            } catch (e: NMDomainException) {
+
             }
         }
         chart_sol.setOnLongClickListener {
@@ -67,9 +73,7 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        chart_err.description.text = "Local errors"
-        chart_sol.description.text = "Numerical and exact solutions"
-        chart_total_err.description.text = "Total Errors"
+        setupPlots()
 
         if (savedInstanceState != null) {
             plotData = savedInstanceState.get("plotdata") as PlotData?
@@ -81,6 +85,27 @@ class MainActivity : AppCompatActivity() {
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         outState.putSerializable("plotdata", plotData)
+    }
+
+    fun setupPlots() {
+        chart_err.description.text = "Local errors"
+        chart_sol.description.text = "Numerical and exact solutions"
+        chart_total_err.description.text = "Total Errors"
+
+        chart_sol.xAxis.position = XAxis.XAxisPosition.BOTTOM
+        chart_sol.xAxis.valueFormatter = ScientificFormatter()
+        chart_sol.axisRight.isEnabled = false
+        chart_sol.axisLeft.valueFormatter = ScientificFormatter()
+
+        chart_err.xAxis.position = XAxis.XAxisPosition.BOTTOM
+        chart_err.xAxis.valueFormatter = ScientificFormatter()
+        chart_err.axisRight.isEnabled = false
+        chart_err.axisLeft.valueFormatter = ScientificFormatter()
+
+        chart_total_err.xAxis.position = XAxis.XAxisPosition.BOTTOM
+        chart_total_err.xAxis.valueFormatter = ScientificFormatter()
+        chart_total_err.axisRight.isEnabled = false
+        chart_total_err.axisLeft.valueFormatter = ScientificFormatter()
     }
 
     private fun drawFun(methodIndex: Int = 0, x0: Double = 0.0, y0: Double = 0.0, x: Double = 0.0, n: Int = 1, recompute: Boolean = true) {
