280b46600ae13afbbc230e59a7dde45a9b2a6602
Artem Bakhanov <artembakhanov@gmail.com>
2019-11-07 18:49:57 +0300

    Fix negative numbers representation

--- app/src/main/java/com/bakhanov/denumericalmethods/numericalMethods/ScientificFormatter.kt
+++ app/src/main/java/com/bakhanov/denumericalmethods/numericalMethods/ScientificFormatter.kt
@@ -2,12 +2,13 @@ package com.bakhanov.denumericalmethods.numericalMethods
 
 import com.github.mikephil.charting.formatter.ValueFormatter
 import java.text.DecimalFormat
+import kotlin.math.absoluteValue
 
 class ScientificFormatter : ValueFormatter() {
     private val format = "0.0000"
 
     override fun getFormattedValue(value: Float): String {
-        return if (value > 1e6)
+        return if (value.absoluteValue > 1e6)
             "%.4e".format(value)
         else
             DecimalFormat(format).format(value.toDouble())
