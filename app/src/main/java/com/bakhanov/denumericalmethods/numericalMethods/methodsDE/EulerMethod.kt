437bf620838614630827fc9e10e7bec36ad17a25
Artem Bakhanov <artembakhanov@gmail.com>
2019-11-09 18:38:15 +0300

    EulerMethod is not open

--- app/src/main/java/com/bakhanov/denumericalmethods/numericalMethods/methodsDE/EulerMethod.kt
+++ app/src/main/java/com/bakhanov/denumericalmethods/numericalMethods/methodsDE/EulerMethod.kt
@@ -2,7 +2,7 @@ package com.bakhanov.denumericalmethods.numericalMethods.methodsDE
 
 import com.bakhanov.denumericalmethods.numericalMethods.Equation
 
-open class EulerMethod(override val equation: Equation) : IterativeMethod(equation) {
+class EulerMethod(override val equation: Equation) : IterativeMethod(equation) {
     override fun next(xi: Double, yi: Double): Double {
         return yi + step * equation.function(xi, yi)
     }
